b5b928bbf4190c07d6441ad8b0a850167bf8b187
corerd <corerd@live.com>
2021-02-11 06:02:15 +0100

    Disable dropdown list after field select

--- docs/assets/js/dropdown.js
+++ docs/assets/js/dropdown.js
@@ -1,9 +1,21 @@
-/*
-  Dropdown management functions
-*/
+/**
+ * W3.CSS Dropdown Classes management functions
+ */
 
 /**
- * Hide element by class
+ * Enable dropdown list on mouseover
+ * 
+ * See: https://stackoverflow.com/a/50206746
+ */
+var element = document.getElementById('drop_select');
+element.addEventListener('mouseover', function() {
+  document.getElementById("drop_fields").style.visibility = 'visible';
+});
+var event = new MouseEvent('mouseover');
+element.dispatchEvent(event);
+
+/**
+ * Draw item by field class and disable dropdown list
  * 
  * @param {*} field_id 
  * @param {*} field_name 
@@ -11,28 +23,28 @@
  * See: https://stackoverflow.com/q/18414384
  */
 function draw_items_grouped_by(field_id, field_name) {
-    document.getElementById("sel_field").innerHTML = field_name;
+  document.getElementById("drop_fields").style.visibility = 'hidden';
 
-    if (field_id != '__all__')
-    {
-      for (let el of document.querySelectorAll('.__all__'))
-      {
-        //el.style.visibility = 'hidden';
-        el.style.display = 'none'
-      }
-    }
+  document.getElementById("sel_field").innerHTML = field_name;
 
-    var treven = 0;
-    for (let el of document.querySelectorAll('.'+field_id))
+  if (field_id != '__all__')
+  {
+    for (let el of document.querySelectorAll('.__all__'))
     {
-      if (treven)
-        el.classList.add("treven");
-      else
-        el.classList.remove("treven");
-        treven = 1 - treven;
-      //el.style.visibility = 'visible';
-      el.style.display = 'table-row'
+      el.style.display = 'none'
     }
+  }
+
+  var treven = 0;
+  for (let el of document.querySelectorAll('.'+field_id))
+  {
+    if (treven)
+      el.classList.add("treven");
+    else
+      el.classList.remove("treven");
+      treven = 1 - treven;
+    el.style.display = 'table-row'
+  }
 }
 
 draw_items_grouped_by('__all__', 'Select an Application Field');
